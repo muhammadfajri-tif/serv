c0949abf6bb7a1dd8e99a5824d1cce45cb266b69
muhammadfajri-tif <149763217+muhammadfajri-tif@users.noreply.github.com>
2024-12-05 17:44:52 +0700

    feat: add support for multi-connection

--- main.c
+++ main.c
@@ -59,49 +59,66 @@ int main(int argc, char *argv[]) {
 
   printf("[INFO] Server waiting for connection...\n");
 
-  // INFO: Step 4: Accept connection
-  struct sockaddr_storage clientinfo;
-  socklen_t addr_size = sizeof(clientinfo);
+  // INFO: Step 4: Always accept connection
+  while (1) {
+    struct sockaddr_storage clientinfo;
+    socklen_t addr_size = sizeof(clientinfo);
 
-  // newfd is a file descriptor for send and receive data to client
-  int newfd = accept(sockfd, (struct sockaddr *)&clientinfo, &addr_size);
-  if (newfd == -1) {
-    perror("[ERRO] Failed to accept connection");
-    exit(1);
-  };
+    // newfd is a file descriptor for send and receive data to client
+    int newfd = accept(sockfd, (struct sockaddr *)&clientinfo, &addr_size);
+    if (newfd == -1) {
+      perror("[ERRO] Failed to accept connection");
+      exit(1);
+    };
 
-  // print client information
-  char client_addr[INET_ADDRSTRLEN];
-  inet_ntop(clientinfo.ss_family, (struct sockaddr *)&clientinfo.ss_family,
-            client_addr, sizeof(client_addr));
-  printf("[INFO] Server got connection from %s\n", client_addr);
+    // log client information
+    char client_addr[INET_ADDRSTRLEN];
+    inet_ntop(clientinfo.ss_family, (struct sockaddr *)&clientinfo.ss_family,
+              client_addr, sizeof(client_addr));
+    printf("\n[INFO] Server got connection from %s\n", client_addr);
 
-  close(sockfd); // no need listener, so we can close socket file descriptor
+    // NOTE: handle message using multi-process
+    if (!fork()) {
+      // no need listener, so we can close socket file descriptor,
+      // and accept next connection
+      close(sockfd);
 
-  // INFO: Step 5.1: Send message
-  char *msg = "Hello socket!\n";
-  int bytes_sent;
+      // INFO: Step 5.1: Send welcome message
+      if (send(newfd, "Welcome!\n", 9, 0) == -1) {
+        perror("[ERRO] Failed to sent message");
+        exit(1);
+      }
 
-  if ((bytes_sent = send(newfd, msg, strlen(msg), 0)) == -1) {
-    perror("[ERRO] Failed to sent message");
-    exit(1);
+      // INFO: Step 5.2: Receive message
+      char buffer_msg[256];
+      int bytes_receive = recv(newfd, buffer_msg, sizeof(buffer_msg), 0);
+      if (bytes_receive == -1) {
+        perror("[ERRO] Failed receive message");
+        exit(1);
+      } else if (bytes_receive == 0) {
+        perror("[WARN] Remote connection already close with status");
+      } else {
+        printf("Receive message from client: %s", buffer_msg);
+      }
+      printf("[INFO] Number of bytes receive from the client: %d\n",
+             bytes_receive);
+
+      // INFO: Step 5.3: Send exit message
+      char *msg = "Thank you for using our service!\n";
+
+      if (send(newfd, msg, strlen(msg), 0) == -1) {
+        perror("[ERRO] Failed to sent message");
+        exit(1);
+      }
+
+      close(newfd);
+      exit(0);
+    }
+
+    close(newfd);
   }
 
-  printf("[INFO] Number of bytes sent: %d\n", bytes_sent);
-
-  // INFO: Step 5.2: Receive message
-  char buffer_msg[256];
-  int bytes_receive = recv(newfd, buffer_msg, sizeof(buffer_msg), 0);
-  if (bytes_receive == -1) {
-    perror("[ERRO] Failed receive message");
-    exit(1);
-  } else if (bytes_receive == 0) {
-    perror("[WARN] Remote connection already close with status");
-  } else {
-    printf("Receive message from client: %s\n", buffer_msg);
-  }
-
-  close(newfd);
+  // unreachable code
 
   return 0;
 }
